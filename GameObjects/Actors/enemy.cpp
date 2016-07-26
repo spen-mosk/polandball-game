01fb37d8096cc25f013a78e2112f94bae8ff4f71
otooisdumb <smdevolt@gmail.com>
2016-07-26 13:59:22 -0700

    enemy adjustments

--- GameObjects/Actors/enemy.cpp
+++ GameObjects/Actors/enemy.cpp
@@ -5,16 +5,21 @@ Enemy::Enemy(int x, int y, EnemyStatistics* stat): Actor(x,y,stat)
     this->stats = stat;
 }
 
-void Enemy::makeDecision(KDTree* tree){
-
-}
-
 void Enemy::update(){
+    if(lockedOn){
+        //stuff
+        if(canAttack()){
+            //stuff
+        }else{
+            //stuff
+        }
+    }
 
 }
 
 void Enemy::draw(QPainter * p){
-
+    p->setBrush(Qt::red);
+    p->drawEllipse(*(getDrawingPosition()), drawingHeight, drawingWidth);
 }
 
 void Enemy::jump(){
@@ -27,6 +32,20 @@ void Enemy::maximizeJump(){
 
 void Enemy::resetJump(){
 
+}
+
+void Enemy::lockOn(Player* p){
+    this->lockedOn = true;
+    this->player = p;
+}
+
+void Enemy::cancelLock(){
+    this->lockedOn = false;
+}
+
+bool Enemy::canAttack(){
+    //bool stuff based on player's position, speed, and size of enemy's attack
+    return true;
 }
 
 Attack* Enemy::generateAttack(){
--- GameObjects/Actors/enemy.h
+++ GameObjects/Actors/enemy.h
@@ -5,24 +5,30 @@
 #include "GameObjects/Attacks/meleeattack.h"
 #include "GameObjects/Attacks/rangedattack.h"
 #include "GameObjects/kdtree.h"
+#include "GameObjects/Actors/player.h"
 
 class Enemy : public Actor
 {
 public:
     Enemy(int,int, EnemyStatistics*);
     Attack* generateAttack();
-    void makeDecision(KDTree*);
     void update();
     void draw(QPainter *);
     void jump();
     void resetJump();
     void maximizeJump();
+    bool canAttack();
+    void lockOn(Player*);
+    void cancelLock();
+
 private:
     //AI to be added/used in makeDecision
     int jumpDelay;
     int jumpDelayCount;
     int verticalSpeed;
     int jumpCount;
+    bool lockedOn;
+    Player* player;
     EnemyStatistics* stats;
 };
 
